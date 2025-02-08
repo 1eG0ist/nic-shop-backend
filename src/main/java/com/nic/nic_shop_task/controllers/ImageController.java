bcaf00d9d132bb71630db9d09c5e6d3e90a92f8d
1eG0ist <diadypetya@yandex.ru>
2025-02-08 13:08:06 +0300

    add logic for getting images

--- src/main/java/com/nic/nic_shop_task/controllers/ImageController.java
+++ src/main/java/com/nic/nic_shop_task/controllers/ImageController.java
@@ -2,13 +2,21 @@ package com.nic.nic_shop_task.controllers;
 
 import com.nic.nic_shop_task.services.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/images")
 public class ImageController {
-//    private final ImageService imageService;
+    private final ImageService imageService;
 
-//    @GetMapping
+    @GetMapping()
+    public ResponseEntity<InputStreamResource> downloadFile(@RequestParam("path") String path) {
+        return imageService.downloadFile(path);
+    }
 }
--- src/main/java/com/nic/nic_shop_task/services/Impl/ImageServiceImpl.java
+++ src/main/java/com/nic/nic_shop_task/services/Impl/ImageServiceImpl.java
@@ -1,6 +1,106 @@
 package com.nic.nic_shop_task.services.Impl;
 
 import com.nic.nic_shop_task.services.ImageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class ImageServiceImpl implements ImageService {
+
+    @Value("${file.files-dir}")
+    private String filesDir;
+    @Value("${file.files-sep}")
+    private String filesSep;
+
+    private void saveFile(MultipartFile file, String filePath) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(filePath)) {
+            fos.write(file.getBytes());
+        }
+    }
+
+    @Override
+    public ResponseEntity<String> uploadFile(MultipartFile file, String category) {
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().body("File is empty");
+        }
+
+        String path = filesDir + category + filesSep
+                + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"))
+                + UUID.randomUUID().toString().substring(0, 10)
+                + ".jpg";
+
+        try {
+            saveFile(file, path);
+            return ResponseEntity.ok(path);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public ResponseEntity<InputStreamResource> downloadFile(String filePath) {
+        try {
+            File file = Paths.get(filePath).toFile();
+            if (!file.exists()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+            InputStreamResource resource = new InputStreamResource(Files.newInputStream(file.toPath()));
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(file.length())
+                    .body(resource);
+
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @Override
+    public ResponseEntity<?> deleteFiles(List<String> filesToDelete) {
+        List<File> successfullyDeletedFiles = new ArrayList<>();
+
+        try {
+            for (String filePath : filesToDelete) {
+                File file = Paths.get(filePath).toFile();
+                if (file.exists() && file.isFile()) {
+                    Files.deleteIfExists(file.toPath());
+                    successfullyDeletedFiles.add(file);
+                }
+            }
+            return ResponseEntity.ok("Files deleted successfully.");
+        } catch (Exception e) {
+            rollbackFiles(successfullyDeletedFiles);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete files: " + e.getMessage());
+        }
+    }
+
+    private void rollbackFiles(List<File> filesToRestore) {
+        for (File file : filesToRestore) {
+            try {
+                Files.createFile(file.toPath());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
