2946965dba34a17627d542fb35e7761d1c15516e
1eG0ist <diadypetya@yandex.ru>
2025-03-04 11:13:32 +0300

    deleted redundant

--- src/main/java/com/nic/nic_shop_task/repositories/ProductCategoriesRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.nic.nic_shop_task.repositories;
-//
-//import com.nic.nic_shop_task.models.ProductCategories;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ProductCategoriesRepository extends CrudRepository<ProductCategory, Long> {
-//}
